0e637ad7bc561220142794ec08f0b887415626b0
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2022-08-29 17:47:54 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import { Counter } from "../src/features/counter/Counter";
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <div>
