66bb0e1cdda71252394d289290ad876ef51fba0f
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2022-09-05 19:05:14 +0100

    set up testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { store } from './store'
-import { Provider } from 'react-redux'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { store } from './store';
 
-const container = document.getElementById('root')
-const root = createRoot(container as HTMLElement)
+const container = document.getElementById('root');
+const root = createRoot(container as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode >
-)
+  </React.StrictMode>,
+);
