0a9c88d0614365f2a6558cba71bad4c8a8d2a1fc
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2022-09-24 13:21:21 +0100

    bring back basic redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+import { Counter } from './features/counter/Counter';
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Counter />
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
   );
 }
