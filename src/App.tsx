c7a3b14d113df7d200b4675960be3ee730bb4f4c
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2022-10-01 07:10:43 +0100

    tidy

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,5 @@ export function App() {
 }
 
 export function WrappedApp() {
-  return (
-    <App />
-  );
+  return <App />
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { WrappedApp } from './App';
 import { store } from './store';
+import { WrappedApp } from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
